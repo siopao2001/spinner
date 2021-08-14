6b838c8327b269ec8076f2c666ceed883c26830a
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-14 16:39:40 +0000

    Created spinner1.js

---
+++ spinner1.js
@@ -0,0 +1,29 @@
+setTimeout(() => {
+ process.stdout.write('\r| ')
+},100)
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+
+  process.stdout.write('\r|   '); 
+}, 1500);
